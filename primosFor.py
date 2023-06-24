39bb9a09ab9f5d59cacff3a4d290ca1a47189e95
ValentinIE <vnesparza46@gmail.com>
2023-06-24 15:51:43 -0600

    Primer commit

--- primosFor.py
+++ primosFor.py
@@ -3,4 +3,12 @@ try:
     n = int(n)
 except:
     n=1
- 
+totaldivisores=0
+totaldivisores=int(totaldivisores)
+for i in range(n):
+    if n%(i+1)==0:
+        totaldivisores+=1
+if totaldivisores==2 or totaldivisores==1:
+    print('SI')
+else:
+    print('NO')
--- primosWhile.py
+++ primosWhile.py
@@ -3,4 +3,15 @@ try:
     n = int(n)
 except:
     n=1
- 
+totaldivisores=0
+totaldivisores=int(totaldivisores)
+contador=n
+contador=int(contador)
+while contador>0:
+    if n%contador==0:
+        totaldivisores+=1
+    contador-=1
+if totaldivisores==2 or totaldivisores==1:
+    print('SI')
+else:
+    print('NO')
